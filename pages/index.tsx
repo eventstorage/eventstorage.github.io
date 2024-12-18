ab1b88e033646cf7798f20af739725764815b728
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-12-18 15:11:14 +0300

    load default site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,57 @@
-import {
-  Button,
-  Card,
-  CardGrid,
-  GuiderLayout,
-  Hero,
-} from '@neato/guider/client';
- 
+import { Button, GuiderLayout } from '@neato/guider/client';
+import { Home } from '../components/home';
+import { HomeCard, HomeCardContainer } from '../components/home-card';
+
+interface Project {
+  href: string;
+  icon: string;
+  title: string;
+  description: string;
+}
+
 export default function LandingPage() {
+  const projects: Project[] = [
+    {
+      title: '@neato/guider',
+      description: 'Flexible documentation that looks good out of the box.',
+      href: '/docs/guider',
+      icon: 'ic:round-menu-book',
+    },
+    {
+      title: '@neato/config',
+      description:
+        'NodeJS configuration loader with strict typing and autocomplete.',
+      href: '/docs/config',
+      icon: 'material-symbols:settings-heart-rounded',
+    },
+  ];
   return (
-    <GuiderLayout meta={{ site: 'main' }}>
-      <Hero>
-        <Hero.Badge title="V1.0.0" to="/docs/guider/guides">
-          Just went out of alpha!
-        </Hero.Badge>
-        <Hero.Title>Documentation that looks great out of the box</Hero.Title>
-        <Hero.Subtitle>
-          Flexible but beautiful documentation, easy to write and easier to
-          extend. Exactly what you need everytime.
-        </Hero.Subtitle>
-        <Hero.Actions>
-          <Button to="/docs/guider/guides">Get started</Button>
-          <Button to="https://github.com/mrjvs/neatojs" type="secondary">
-            View on GitHub
-          </Button>
-        </Hero.Actions>
-      </Hero>
-      <CardGrid>
-        <Card icon="mdi:cube-outline" title="Focus on writing">
-          Effortlessly create beautiful documentation sites with just markdown.
-        </Card>
-        <Card icon="mdi:cube-outline" title="Focus on writing">
-          Effortlessly create beautiful documentation sites with just markdown.
-        </Card>
-        <Card icon="mdi:cube-outline" title="Focus on writing">
-          Effortlessly create beautiful documentation sites with just markdown.
-        </Card>
-      </CardGrid>
+    <GuiderLayout meta={{ layout: 'page' }}>
+      <Home.Container>
+        <Home.Title>
+          NeatoJS — A collection of libraries made to simplify
+        </Home.Title>
+        <Home.Subtitle>
+          Tools that follow the philosophy of doing only one thing, and doing it
+          right!
+        </Home.Subtitle>
+        <HomeCardContainer>
+          {projects.map((v) => (
+            <HomeCard.Card
+              key={v.href}
+              icon={v.icon}
+              right={
+                <Button to={v.href} type="secondary">
+                  View project
+                </Button>
+              }
+            >
+              <HomeCard.Title href={v.href}>{v.title}</HomeCard.Title>
+              <HomeCard.Description>{v.description}</HomeCard.Description>
+            </HomeCard.Card>
+          ))}
+        </HomeCardContainer>
+      </Home.Container>
     </GuiderLayout>
   );
 }
